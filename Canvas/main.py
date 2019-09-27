a226fe79185f5962dfaf76ef2bb384cbd9a5e5e5
VitalMartell <55775289+VitalMartell@users.noreply.github.com>
2019-09-27 06:06:44 +0300

    update

--- Canvas/main.py
+++ Canvas/main.py
@@ -1,41 +1,153 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.config import Config
-Config.set('graphics', 'resizable', 0)
+from math import sqrt
+from random import random
+from kivy.graphics import Color, Rectangle, Point, GraphicException
+Config.set('graphics', 'resizable', 1)
 Config.set('graphics', 'width', 400)
 Config.set('graphics', 'height', 500)
 Config.write()
 
-#Window.size = (800, 500)
+Window.size = (800, 500)
 #Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
-
+def calculate_points(x1, y1, x2, y2, steps=5):
+    dx = x2 - x1
+    dy = y2 - y1
+    dist = sqrt(dx * dx + dy * dy)
+    if dist < steps:
+        return
+    o = []
+    m = dist / steps
+    for i in range(1, int(m)):
+        mi = i / m
+        lastx = x1 + dx * mi
+        lasty = y1 + dy * mi
+        o.extend([lastx, lasty])
+    return o
 
 class Container(BoxLayout):
     def points(self):
-        point_list = [0, 0, 0, 50, -10, 60, -50, 60]
+        point_list= []
+        list_X = [0, 0, -10, -50]
+        list_Y = [0, 50, 60, 60]
         x0 = self.zero_point()
         x0 = x0[0]
         y0 = self.zero_point()
         y0 = y0[1]
-        for i in range(0, len(point_list), 2):
-            point_list[i] += x0
-        for i in range(1, len(point_list), 2):
-            point_list[i] += y0
+        for i in range(0, len(list_X)):
+            list_X[i] += x0
+        for i in range(0, len(list_Y)):
+            list_Y[i] += y0
+        if len(list_X) == len(list_Y):
+            for i in range(len(list_X)):
+                point_list.extend([list_X[i], list_Y[i]])
+        else:
+            point_list = []
         return point_list
 
-    def params(self):
-        cnt = Window.width/2 - 400/2, Window.height/2-200/2, 400, 200
-        return cnt
 
     def zero_point(self):
         x0 = Window.width/2
         y0 = Window.height/2
-        xy0 = x0, y0, x0 + 0, y0 + 50
+        xy0 = x0, y0
         return xy0
+
+
+    def on_touch_down(self, touch):
+        win = self.get_parent_window()
+        ud = touch.ud
+        ud['group'] = g = str(touch.uid)
+        pointsize = 5
+        if 'pressure' in touch.profile:
+            ud['pressure'] = touch.pressure
+            pointsize = (touch.pressure * 100000) ** 2
+        ud['color'] = random()
+
+        with self.canvas:
+            Color(ud['color'], 1, 1, mode='hsv', group=g)
+            ud['lines'] = [
+                Rectangle(pos=(touch.x, 0), size=(1, win.height), group=g),
+                Rectangle(pos=(0, touch.y), size=(win.width, 1), group=g),
+                Point(points=(touch.x, touch.y), source='particle.png',
+                      pointsize=pointsize, group=g)]
+
+        ud['label'] = Label(size_hint=(None, None))
+        self.update_touch_label(ud['label'], touch)
+        Window.add_widget(ud['label'])
+        touch.grab(self)
+        return True
+
+    def on_touch_move(self, touch):
+        if touch.grab_current is not self:
+            return
+        ud = touch.ud
+        ud['lines'][0].pos = touch.x, 0
+        ud['lines'][1].pos = 0, touch.y
+
+        index = -1
+
+        while True:
+            try:
+                points = ud['lines'][index].points
+                oldx, oldy = points[-2], points[-1]
+                break
+            except:
+                index -= 1
+
+        points = calculate_points(oldx, oldy, touch.x, touch.y)
+
+        # if pressure changed create a new point instruction
+        if 'pressure' in ud:
+            if not .95 < (touch.pressure / ud['pressure']) < 1.05:
+                g = ud['group']
+                pointsize = (touch.pressure * 100000) ** 2
+                with self.canvas:
+                    Color(ud['color'], 1, 1, mode='hsv', group=g)
+                    ud['lines'].append(
+                        Point(points=(), source='particle.png',
+                              pointsize=pointsize, group=g))
+
+        if points:
+            try:
+                lp = ud['lines'][-1].add_point
+                for idx in range(0, len(points), 2):
+                    lp(points[idx], points[idx + 1])
+            except GraphicException:
+                pass
+
+        ud['label'].pos = touch.pos
+        import time
+        t = int(time.time())
+        if t not in ud:
+            ud[t] = 1
+        else:
+            ud[t] += 1
+        self.update_touch_label(ud['label'], touch)
+
+    def on_touch_up(self, touch):
+        if touch.grab_current is not self:
+            return
+        touch.ungrab(self)
+        ud = touch.ud
+        self.canvas.remove_group(ud['group'])
+        Window.remove_widget(ud['label'])
+
+    def update_touch_label(self, label, touch):
+        dif = self.zero_point()
+        dif_x = dif[0]
+        dif_y = dif[1]
+        label.text = 'ID: %s\nPos XY: (%d, %d)\nClass: %s' % (
+            touch.id, touch.x - dif_x, touch.y - dif_y, touch.__class__.__name__)
+        label.texture_update()
+        label.pos = touch.pos
+        label.size = label.texture_size[0] + 20, label.texture_size[1] + 20
 
 
 class CanvasApp(App):
--- Listings/ComicCreator/comicwidgets.py
+++ Listings/ComicCreator/comicwidgets.py
@@ -7,40 +7,40 @@ class DraggableWidget(RelativeLayout):
         self.selected = None
         super(DraggableWidget, self).__init__(**kwargs)
 
-def on_touch_down(self, touch):
-    if self.collide_point(touch.x, touch.y):
-        self.select()
-        return True
-    return super(DraggableWidget, self).on_touch_down(touch)
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.x, touch.y):
+            self.select()
+            return True
+        return super(DraggableWidget, self).on_touch_down(touch)
 
-def select(self):
-    if not self.selected:
-        self.ix = self.center_x
-        self.iy = self.center_y
-        with self.canvas:
-            self.selected = Line(rectangle=(0,0,self.width,self.height), dash_offset=2)
+    def select(self):
+        if not self.selected:
+            self.ix = self.center_x
+            self.iy = self.center_y
+            with self.canvas:
+                self.selected = Line(rectangle=(0,0,self.width,self.height), dash_offset=2)
 
-def on_touch_move(self, touch):
-    (x,y) = self.parent.to_parent(touch.x, touch.y)
-    if self.selected and self.parent.collide_point(x -self.width/2, y - self.height/2):
-        self.translate(touch.x-self.ix,touch.y-self.iy)
-        return True
-    return super(DraggableWidget, self).on_touch_move(touch)
+    def on_touch_move(self, touch):
+        (x,y) = self.parent.to_parent(touch.x, touch.y)
+        if self.selected and self.parent.collide_point(x -self.width/2, y - self.height/2):
+            self.translate(touch.x-self.ix,touch.y-self.iy)
+            return True
+        return super(DraggableWidget, self).on_touch_move(touch)
 
-def translate(self, x, y):
-    self.center_x = self.ix = self.ix + x
-    self.center_y = self.iy = self.iy + y
+    def translate(self, x, y):
+        self.center_x = self.ix = self.ix + x
+        self.center_y = self.iy = self.iy + y
 
-def on_touch_up(self, touch):
-    if self.selected:
-        self.unselect()
-        return True
-    return sup
-    
-def unselect(self):
-    if self.selected:
-        self.canvas.remove(self.selected)
-        self.selected = None
+    def on_touch_up(self, touch):
+        if self.selected:
+            self.unselect()
+            return True
+        return sup
+        
+    def unselect(self):
+        if self.selected:
+            self.canvas.remove(self.selected)
+            self.selected = None
 
 class StickMan(DraggableWidget):
     pass
