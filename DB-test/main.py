022b2a302ecdfcde82173dde79a9b80ac82a81f8
VitalMartell <55775289+VitalMartell@users.noreply.github.com>
2019-10-04 06:39:14 +0300

    update

--- DB-test/main.py
+++ DB-test/main.py
@@ -8,13 +8,20 @@ from kivy.properties import ObjectProperty
 
 from kivy.config import Config
 Config.set('graphics', 'resizable', 0)
-Config.set('graphics', 'width', 500)
+Config.set('graphics', 'width', 600)
 Config.set('graphics', 'height', 600)
 #Config.set('kivy','keyboard_mode','systemanddock')
 
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\user\Desktop\DB-2017.accdb;')
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=SSBSERVER\TAS;'
+                      'Database=TAS;'
+                      'uid=user;pwd=user;')
+
+#conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, #*.accdb)};DBQ=C:\Users\user\Desktop\DB-2017.accdb;')
+
 cursor = conn.cursor()
-cursor.execute('select username,password from users')
+cursor.execute('select compid , barcodeid from TMS.TAS_COMPMORE')
+#cursor.execute('select username,password from users')
 
 
     
@@ -22,18 +29,22 @@ class Container(Widget):
     txt_input = ObjectProperty()
 
     def get_text(self):
-        b = []
         i = 1
         c = ""
         rows = cursor.fetchall()
+        #print(rows)
         for row in rows:
-            #print (row.username)
-            a = str(row.username + "\t\t"+ row.password)
-            c += str(i) + "\t\t" +a + "\n"
-            i += 1
-        print(c)
-            #b.append(a)
-            #print (b)
+            try:
+                if row.barcodeid is None:
+                    barcode = "0"
+                else:
+                    barcode = row.barcodeid
+                a = str(row.compid + "\t\t"+ barcode)
+                c += str(i) + "\t\t" + a  + "\n"
+                i += 1
+                #print(c)
+            except:
+                c = "ERROR"
         csr = conn.cursor()
         csr.close()
         conn.close()
